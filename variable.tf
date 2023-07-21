c58aede2ec039e08d3b18381f3c7bd27b4b5a285
Yadav <vijay.g.yadav@accenture.com>
2023-07-21 14:14:52 -0400

    Terrafrom module

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name" {
+  type        = string
+  description = "resocurce group name"
+}
+variable "location" {
+  type        = string
+  description = "Location "
+}
+
+variable "storageaccountname" {
+  type        = string
+  description = "Account name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Envirnment"
+}
